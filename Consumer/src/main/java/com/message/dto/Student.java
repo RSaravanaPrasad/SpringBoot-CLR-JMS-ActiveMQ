0020ae8a00cabe32f6757dcc9fa7f6638f0773a8
RSaravanaPrasad <88064225+RSaravanaPrasad@users.noreply.github.com>
2021-09-21 18:24:57 +0530

    Lombok annotation added for immutability
    Lombok annotation added for immutability

--- Consumer/src/main/java/com/message/dto/Student.java
+++ Consumer/src/main/java/com/message/dto/Student.java
@@ -1,17 +1,22 @@
 package com.message.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Value;
 
 import java.beans.ConstructorProperties;
 import java.io.Serializable;
 
+@Value
 public final class Student implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+
 	@JsonProperty
 	private final String studentId;
+
 	@JsonProperty
 	private final String name;
+
 	@JsonProperty
 	private final String rollNumber;
 
@@ -20,18 +25,6 @@ public final class Student implements Serializable {
 		this.studentId = studentId;
 		this.name = name;
 		this.rollNumber = rollNumber;
-	}
-
-	public String getStudentId() {
-		return studentId;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public String getRollNumber() {
-		return rollNumber;
 	}
 
 }
